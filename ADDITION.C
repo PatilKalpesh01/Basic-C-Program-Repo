3cf35a8c52c1113eba66fb29cac7a26df2d89c2a
PatilKalpesh01 <kp1422682@gimal.com>
2022-03-23 09:31:39 +0530

    ADDED:[kalpeshpatil] my c program for addition

---
+++ ADDITION.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("\n Enter the 2 no=");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("\n addition=%d",c);
+	getch();
+}
