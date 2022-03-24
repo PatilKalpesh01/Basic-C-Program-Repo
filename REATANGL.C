0c7112e3c7139beccdf76e1a21248c8c310ef771
PatilKalpesh01 <kp1422682@gimal.com>
2022-03-24 09:39:41 +0530

    ADDED:[KALPESH] Program for reatangle

---
+++ REATANGL.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float a,b,e=0;
+	clrscr();
+	printf("\n Enter the length of rectangle");
+	csanf("%f",&a);
+	printf("\n Enter the breadth of rectangte");
+	scanf("%f",&b);
+	c=a*b;
+	printf("\n Area of rectangte=%f",c);
+	getch();
+
+}
